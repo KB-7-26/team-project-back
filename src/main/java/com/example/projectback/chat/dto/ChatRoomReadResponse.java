6b03b9427a3a388c7bed0301971505d66965d5ad
catlover <jimini1183@naver.com>
2026-06-07 21:30:00 +0900

    [WK-25] feat: 채팅 읽음 처리 응답 개선

---
+++ src/main/java/com/example/projectback/chat/dto/ChatRoomReadResponse.java
@@ -0,0 +1,12 @@
+package com.example.projectback.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ChatRoomReadResponse {
+    private LocalDateTime opponentLastReadAt;
+}
