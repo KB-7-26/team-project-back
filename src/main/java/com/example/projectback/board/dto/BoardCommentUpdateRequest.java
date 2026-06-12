5b08be1cb80a6c0cd3a88664abab07d81b19b380
kskim <kskim0109@gmail.com>
2026-06-12 14:13:25 +0900

    feat: 댓글 최대 글자 수 제한 유효성 검증 추가

--- src/main/java/com/example/projectback/board/dto/BoardCommentUpdateRequest.java
+++ src/main/java/com/example/projectback/board/dto/BoardCommentUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.example.projectback.board.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BoardCommentUpdateRequest {
     @NotBlank(message = "댓글 내용을 입력해주세요.")
+    @Size(max = 500, message = "댓글은 500자 이내로 입력해주세요.")
     private String content;
 }
